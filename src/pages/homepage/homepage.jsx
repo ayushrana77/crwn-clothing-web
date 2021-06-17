7377444acff9870cdb73e0e0e8e3334629cf651a
Ayush Rana <ayushrana200177@gmail.com>
2021-06-17 19:38:26 +0530

    broke compents into smaller compontes,finshed styling our menu-item

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Directory from '../../components/directory/directory'
+import './homepage.style.scss'
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  )
+}
+
+export default HomePage
