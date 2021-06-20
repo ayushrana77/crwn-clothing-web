d4d97a5e202154a468d96eaa22753115176d47c6
Ayush Rana <ayushrana200177@gmail.com>
2021-06-20 17:34:17 +0530

    update our menu-item t navtion withrourter HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css'
+import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage'
+
+const HatsPage = () => (
+  <div>
+    <h1> Hats Page</h1>
+  </div>
+)
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   )
 }
