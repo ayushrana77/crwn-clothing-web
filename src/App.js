317ac12ac70f2452f54c8be0109186e8256b85c3
Ayush Rana <ayushrana200177@gmail.com>
2021-06-26 12:04:49 +0530

    upading app redirec if user signin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage'
 import ShopPage from './pages/shop/shop'
 import { connect } from 'react-redux'
@@ -35,13 +35,26 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     )
   }
 }
+const mapStateProps = ({ user }) => ({
+  currentUser: user.currentUser,
+})
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 })
-export default connect(null, mapDispatchToProps)(App)
+export default connect(mapStateProps, mapDispatchToProps)(App)
