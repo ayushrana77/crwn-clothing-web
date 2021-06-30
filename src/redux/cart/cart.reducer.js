16bccb7c9fd22257c9d9498e3bd90cae9557f508
Ayush Rana <ayushrana200177@gmail.com>
2021-06-30 23:04:22 +0530

    added addItem and removing item functionality to checkout componrnt

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,5 @@
-import { stat } from '@nodelib/fs.stat'
 import CartActionTypes from './cart.types'
-import { addItemTOCart } from './cart.utils'
+import { addItemTOCart, removeItemFromCart } from './cart.utils'
 const INITIAL_STATE = {
   hidden: true,
   cartItems: [],
@@ -16,6 +15,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemTOCart(state.cartItems, action.payload),
+      }
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
+      }
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
       }
     default:
       return state
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,3 +11,20 @@ export const addItemTOCart = (cartItems, cartItemToAdd) => {
   }
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (CartItem) => CartItem.id === cartItemToRemove.id
+  )
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id)
+  }
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? {
+          ...cartItem,
+          quantity: cartItem.quantity - 1,
+        }
+      : cartItem
+  )
+}
